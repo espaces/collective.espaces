c5761c9046e1625a31d1aa2db4b3aa03a9786ef1
David\ Beitey <david@davidjb.com>
2013-10-09 16:19:51 +1000

    Hide permission inheritance setting from content in spaces

--- src/collective/espaces/browser/sharing.py
+++ src/collective/espaces/browser/sharing.py
@@ -1,5 +1,7 @@
 from plone.app.workflow.browser.sharing import SharingView as OriginalSharingView, AUTH_GROUP
 from plone.memoize.instance import memoize
+from plone.app.layout.navigation.root import getNavigationRootObject
+from collective.spaces.interfaces import ISpace
 
 class SharingView(OriginalSharingView):
 
@@ -12,3 +14,9 @@ class SharingView(OriginalSharingView):
         info = super(SharingView, self).existing_role_settings()
         result =  filter(lambda v: v['id'] != AUTH_GROUP, info)
         return result
+
+    def can_edit_inherit(self):
+        """ Allow inherited roles on everything except content in Spaces.
+        """
+        navigation_root = getNavigationRootObject(self.context, None)
+        return not ISpace.providedBy(navigation_root)
