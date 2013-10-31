e8798bccf9e486f4787856601d28c2801a00dacb
David\ Beitey <david@davidjb.com>
2013-10-31 18:37:09 +1000

    Allow managers to change sharing permissions for all logged in users.

--- src/collective/espaces/browser/sharing.py
+++ src/collective/espaces/browser/sharing.py
@@ -1,3 +1,5 @@
+from AccessControl import getSecurityManager
+from Products.CMFCore.permissions import ManagePortal
 from plone.app.workflow.browser.sharing import SharingView as OriginalSharingView, AUTH_GROUP
 from plone.memoize.instance import memoize
 from plone.app.layout.navigation.root import getNavigationRootObject
@@ -5,15 +7,24 @@ from collective.spaces.interfaces import ISpace
 
 class SharingView(OriginalSharingView):
 
-    STICKY = ()
+    @property
+    def STICKY(self):
+        sticky = ()
+        security = getSecurityManager()
+        if security.checkPermission(ManagePortal, self.context):
+           sticky = (AUTH_GROUP,)
+        return sticky
 
     @memoize
     def existing_role_settings(self):
         """ Hide the 'Logged in users' entry from the Sharing page.
         """
         info = super(SharingView, self).existing_role_settings()
-        result =  filter(lambda v: v['id'] != AUTH_GROUP, info)
-        return result
+        security = getSecurityManager()
+        if security.checkPermission(ManagePortal, self.context):
+            return info
+        else:
+            return filter(lambda v: v['id'] != AUTH_GROUP, info)
 
     def can_edit_inherit(self):
         """ Allow inherited roles on everything except content in Spaces.
