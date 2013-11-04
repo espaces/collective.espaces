c9015f0b6b552fa0beb5e65d36e7f967076fe6dd
David\ Beitey <david@davidjb.com>
2013-11-04 18:16:45 +1000

    Add password reset button to form

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,5 +4,7 @@ Changelog
 0.1-dev (unreleased)
 --------------------
 
+- Add password regeneration option to Reset Password page.
+  [davidjb]
 - Package created using templer
   [JCU eResearch Centre]
--- src/collective/espaces/browser/views.py
+++ src/collective/espaces/browser/views.py
@@ -1,10 +1,34 @@
 from five import grok
+from zope.formlib import form
+from collective.aaf.auth import _generatePassword
 from collective.spaces.browser.createform import CreateSpaceForm
+from plone.app.users.browser.personalpreferences \
+        import PasswordAccountPanel as OriginalPasswordAccountPanel
+
 
 grok.templatedir('.')
+
 
 class CustomCreateSpaceForm(CreateSpaceForm):
     """ Extend and customise the Space creation form.
     """
     grok.template('create-space')
+
+
+class PasswordAccountPanel(OriginalPasswordAccountPanel):
+
+    actions = form.Actions(
+        form.Action('Change password',
+                    success='action_reset_passwd',
+                    validator='validate_password',
+                    name=u'reset_passwd'),
+        form.Action('Generate new password',
+                    success='action_generate_passwd',
+                    validator=lambda form, action, data: [],
+                    name=u'generate_passwd')
+    )
+
+    def action_generate_passwd(self, action, data):
+        data['new_password'] = _generatePassword(self.context)
+        return self.action_reset_passwd.success_handler(self, action, data)
 
